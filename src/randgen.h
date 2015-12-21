96076d6af990316cff93fb624a57c346ad05c7eb
Roberto Metere <roberto@metere.it>
2015-12-21 15:11:09 +0100

    Random generator interface

---
+++ src/randgen.h
@@ -0,0 +1,24 @@
+/*
+ * A random generator abstract function
+ * 
+ * Copyright (C) 2015  Roberto Metere, Glasgow <roberto.metere@strath.ac.uk>
+ */
+
+#ifndef RANDOM_GENERATOR_H
+#define RANDOM_GENERATOR_H
+//-----------------------------------------------------------------------------
+
+template <class T> class RandomGenerator
+{
+protected:
+  T seed;
+  
+public:
+  virtual ~RandomGenerator() {}
+  
+  virtual void setSeed(T seed) = 0;
+  virtual T next() const = 0;
+};
+//-----------------------------------------------------------------------------
+
+#endif // RANDOM_GENERATOR_H
