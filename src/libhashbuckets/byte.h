9066ce4dd548bac91953d5c6595e7a8346f0ba7a
Roberto Metere <roberto@metere.it>
2016-01-08 01:39:42 +0000

    Separated byte definition

---
+++ src/libhashbuckets/byte.h
@@ -0,0 +1,16 @@
+/*
+ * String union with integers
+ * 
+ * Copyright (C) 2015  Roberto Metere, Glasgow <roberto.metere@strath.ac.uk>
+ */
+
+#ifndef BYTE_TYPE_H
+#define BYTE_TYPE_H
+//-----------------------------------------------------------------------------
+
+#ifndef byte
+typedef unsigned char byte;
+#endif
+//-----------------------------------------------------------------------------
+
+#endif // BYTE_TYPE_H
