9acc52954d691b08ad22b863f6bebe03c5ca90dd
Roberto Metere <roberto.metere@strath.ac.uk>
2016-02-16 19:39:56 +0000

    Default message is of type unknown

--- src/libhashbuckets/eopsimsg.h
+++ src/libhashbuckets/eopsimsg.h
@@ -16,11 +16,13 @@
  * Type of message in the EO-PSI protocol
  */
 typedef enum EOPSIMessageType {
-  EOPSI_MESSAGE_OUTSOURCING_DATA,            ///< Data to store into the cloud (server)
-  EOPSI_MESSAGE_CLIENT_COMPUTATION_REQUEST,  ///< Client outsource computation to another client
-  EOPSI_MESSAGE_CLOUD_COMPUTATION_REQUEST,   ///< Client outsource computation to server
-  EOPSI_MESSAGE_OUTPUT_COMPUTATION,          ///< Server/Cloud outputs some computation
-  EOPSI_MESSAGE_POLYNOMIAL,                  ///< Client sends a polynomial to another client as "glasses" for the output from the cloud
+    EOPSI_MESSAGE_OUTSOURCING_DATA            ///< Data to store into the cloud (server)
+  , EOPSI_MESSAGE_CLIENT_COMPUTATION_REQUEST  ///< Client outsources computation to another client
+  , EOPSI_MESSAGE_CLOUD_COMPUTATION_REQUEST   ///< Client outsources computation to server
+  , EOPSI_MESSAGE_OUTPUT_COMPUTATION          ///< Server/Cloud outputs some computation
+  , EOPSI_MESSAGE_POLYNOMIAL                  ///< Client sends a polynomial to another client as "glasses" for the output from the cloud
+  
+  , EOPSI_MESSAGE_UNKNOWN                     ///< Unknown message type (default)
 } EOPSIMessageType;
 //-----------------------------------------------------------------------------
 
@@ -35,7 +37,7 @@ protected:
   size_t len;
   
 public:
-  EOPSIMessage(void* data = nullptr, const size_t length = 0, const EOPSIMessageType type = EOPSI_MESSAGE_OUTSOURCING_DATA);
+  EOPSIMessage(void* data = nullptr, const size_t length = 0, const EOPSIMessageType type = EOPSI_MESSAGE_UNKNOWN);
   virtual ~EOPSIMessage();
   
   virtual void setPartyId(const std::string& id);
