72d1f2b65b42fd96b61f385f2fa5418a8ca78136
Roberto Metere <roberto@metere.it>
2016-03-08 23:28:21 +0000

    Doc example with seed

--- res/examples/randomzzgen.cpp
+++ res/examples/randomzzgen.cpp
@@ -2,18 +2,19 @@
 
 int main() {
   ZZPRF zzprf;
-  NTL::ZZ z;
+  NTL::ZZ z, seed;
   size_t bits;
   
-  std::cout << "Insert the number of bits for each number to generate: ";
+  std::cout << "Insert the (maximum) number of bits for each number to generate: ";
   std::cin >> bits;
   
-  std::cout << "Generation of 10 random numbers with at most"
+  std::cout << "Generation of 10 random numbers with at most "
             << bits << " bits: " << std::endl;
   
+  seed = zzprf.randomSeed();
   for (size_t i = 0; i < 10; i++) {
-    z = zzprf.generate(i, bits);
-    std::cout << (i + 1) << " - " << z << "(" << NTL::NumBits(z) << " bits)" << std::endl;
+    z = zzprf.generate(seed, i, bits);
+    std::cout << (i + 1) << " - " << z << "\t(" << NTL::NumBits(z) << " bits)" << std::endl;
   }
   
   return 0;
