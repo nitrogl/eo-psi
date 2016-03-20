2a0bcd7e9f6f78f646d6c1821923e0ef2fbab8e4
Roberto Metere <roberto@metere.it>
2016-03-20 07:59:08 +0000

    Added pause functionality and stat functions (avg, stddev)

--- src/libhashbuckets/simplebm.cpp
+++ src/libhashbuckets/simplebm.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <iostream>
+#include <math.h>
 #include "simplebm.h"
 //-----------------------------------------------------------------------------
 
@@ -13,6 +14,7 @@ SimpleBenchmark::SimpleBenchmark(const size_t tSize) {
   // Init vector of times and internal state of the benchmark
   this->times.reserve(tSize);
   this->reset();
+  this->stopped = true;
 }
 //-----------------------------------------------------------------------------
 
@@ -21,7 +23,13 @@ SimpleBenchmark::~SimpleBenchmark() {
 //-----------------------------------------------------------------------------
 
 void SimpleBenchmark::takeTime(const std::string cursor) {
+  this->takeTime(false, cursor);
+}
+//-----------------------------------------------------------------------------
+
+void SimpleBenchmark::takeTime(const bool pausing, const std::string cursor) {
   this->times.push_back(std::chrono::high_resolution_clock::now());
+  this->pauses.push_back(pausing);
   if (cursor != "") {
     this->cursors[cursor] = this->times.size() - 1;
   }
@@ -43,6 +51,19 @@ size_t SimpleBenchmark::getNumericCursor(const std::string cursor) const {
 }
 //-----------------------------------------------------------------------------
 
+std::chrono::microseconds SimpleBenchmark::sumNotPausedTimes(size_t fromCursor, size_t toCursor) const {
+  std::chrono::microseconds sum;
+  
+  sum = SimpleBenchmark::ZEROMS;
+  for (size_t i = fromCursor; i < toCursor; i++) {
+    if (!this->pauses[i]) {
+      sum += std::chrono::duration_cast<std::chrono::microseconds>(this->times[i] - this->times[i - 1]);
+    }
+  }
+  return sum;
+}
+//-----------------------------------------------------------------------------
+
 void SimpleBenchmark::start() {
   this->reset();
   this->takeTime();
@@ -50,6 +71,19 @@ void SimpleBenchmark::start() {
 //-----------------------------------------------------------------------------
 
 void SimpleBenchmark::step(const std::string cursor) {
+  // Automatically start if no times are taken
+  if (this->times.size() == 0) {
+    if (cursor != "") {
+      std::cerr << "step(). WARNING: Impossible to associate any cursor to start time." << std::endl;
+    }
+    this->start();
+    return;
+  }
+  
+  if (this->isPaused() && cursor != "") {
+    std::cerr << "step(). WARNING: Impossible to associate any cursor just after a pause." << std::endl;
+  }
+  
   if (!this->stopped) {
     this->takeTime(cursor);
   }
@@ -59,13 +93,59 @@ void SimpleBenchmark::step(const std::string cursor) {
 void SimpleBenchmark::reset() {
   this->times.clear();
   this->cursors.clear();
+  this->pauses.clear();
+  this->pauses.push_back(false);
   this->stopped = false;
 }
 //-----------------------------------------------------------------------------
 
+void SimpleBenchmark::pause(const std::string cursor) {
+  // Do nothing if chrono has not started
+  if (this->times.size() == 0) {
+    std::cerr << "pause(). You have to start it first. Nothing to do." << std::endl;
+    return;
+  }
+  
+  // Silently ignore double pause
+  if (this->isPaused()) {
+    return;
+  }
+  
+  if (!this->stopped) {
+    this->takeTime(true, cursor);
+  }
+}
+//-----------------------------------------------------------------------------
+
+void SimpleBenchmark::cont() {
+  step();
+}
+//-----------------------------------------------------------------------------
+
 void SimpleBenchmark::stop(const std::string cursor) {
-  this->takeTime(cursor);
+  if (this->stopped) {
+    std::cerr << "stop(). You should start before stopping." << std::endl;
+    return;
+  }
+  // Do not take the time in pause
+  if (!this->isPaused()) {
+    this->takeTime(cursor);
+  } else {
+    if (cursor != "") {
+      std::cerr << "stop(). Ignoring cursor in pause condition." << std::endl;
+    }
+  }
   this->stopped = true;
+}
+//-----------------------------------------------------------------------------
+
+bool SimpleBenchmark::isPaused() const {
+  return !this->stopped && this->pauses[this->pauses.size() - 1];
+}
+//-----------------------------------------------------------------------------
+
+bool SimpleBenchmark::isStopped() const {
+  return this->stopped;
 }
 //-----------------------------------------------------------------------------
 
@@ -87,19 +167,67 @@ std::chrono::microseconds SimpleBenchmark::benchmark(const std::string cursor) c
 }
 //-----------------------------------------------------------------------------
 
-std::chrono::microseconds SimpleBenchmark::cumulativeBenchmark(const size_t cursor) const {
+std::chrono::microseconds SimpleBenchmark::cumulativeBenchmark(const size_t cursor, const bool noPauses) const {
   if (cursor >= this->times.size()) {
     return SimpleBenchmark::ZEROMS;
   }
   if (cursor < 1) {
-    return std::chrono::duration_cast<std::chrono::microseconds>(this->times[this->times.size() - 1] - this->times[0]);
+    if (noPauses) {
+      return this->sumNotPausedTimes(1, this->times.size());
+    } else {
+      return std::chrono::duration_cast<std::chrono::microseconds>(this->times[this->times.size() - 1] - this->times[0]);
+    }
   } else {
-    return std::chrono::duration_cast<std::chrono::microseconds>(this->times[cursor] - this->times[0]);
+    if (noPauses) {
+      return this->sumNotPausedTimes(1, cursor + 1);
+    } else {
+      return std::chrono::duration_cast<std::chrono::microseconds>(this->times[cursor] - this->times[0]);
+    }
   }
 }
 //-----------------------------------------------------------------------------
 
-std::chrono::microseconds SimpleBenchmark::cumulativeBenchmark(const std::string cursor) const {
-  return this->cumulativeBenchmark(this->getNumericCursor(cursor));
+std::chrono::microseconds SimpleBenchmark::cumulativeBenchmark(const std::string cursor, const bool noPauses) const {
+  return this->cumulativeBenchmark(this->getNumericCursor(cursor), noPauses);
+}
+//-----------------------------------------------------------------------------
+
+double SimpleBenchmark::average() const {
+  if (this->times.size() < 2) {
+    std::cerr << "average(). Not enough points to average." << std::endl;
+    return 0.;
+  }
+  return this->cumulativeBenchmark().count() / (double) (this->times.size() - 1);
+}
+//-----------------------------------------------------------------------------
+
+double SimpleBenchmark::variance() const {
+  double avg, var, diff;
+  size_t n;
+  
+  if (this->times.size() < 2) {
+    std::cerr << "variance(). Not enough points to compute standard deviation." << std::endl;
+    return 0.;
+  }
+  
+  avg = this->average();
+  n = this->times.size() - 1;
+  var = 0.;
+  for (size_t i = 1; i < this->times.size(); i++) {
+    diff = std::chrono::duration_cast<std::chrono::microseconds>(this->times[i] - this->times[i - 1]).count() - avg;
+    var += diff/n*diff;
+  }
+  
+  return var;
+}
+//-----------------------------------------------------------------------------
+
+double SimpleBenchmark::standardDeviation() const {
+  if (this->times.size() < 2) {
+    std::cerr << "standardDeviation(). Not enough points to compute standard deviation." << std::endl;
+    return 0.;
+  }
+  
+  return sqrt(this->variance());
 }
 //-----------------------------------------------------------------------------
--- src/libhashbuckets/simplebm.h
+++ src/libhashbuckets/simplebm.h
@@ -21,24 +21,26 @@
  * It is a chronometer with the following functionality:
  * <ul>
  *  <li><strong>start()</strong>. First time taken, it starts the chronometer.</li>
- *  <li><strong>step()</strong>. Intermediate time taken. It creates a time step without stopping the whole chronometer.</li>
+ *  <li><strong>step()</strong>. Intermediate time taken. It creates a time step without stopping the chronometer.</li>
+ *  <li><strong>pause()</strong>. Intermediate time taken. It creates a time step pausing the chronometer.</li>
+ *  <li><strong>cont()</strong>. Continue after a pause.</li>
  *  <li><strong>stop()</strong>. Last time taken, it stops the chronometer.</li>
  *  <li><strong>reset()</strong>. Reset the chronometer at its initial state.</li>
  * </ul>
  */
 class SimpleBenchmark {
 private:
-  size_t timeSize; ///< It is an initial size to optimise successive calls of step()
-  
   /**
    * Take the current time.
    */
-  void takeTime(const std::string cursor = "");
+  void takeTime(const std::string cursor);
+  void takeTime(const bool pausing = false, const std::string cursor = "");
   
 protected:
-  std::vector<std::chrono::high_resolution_clock::time_point> times;             ///< All the times taken.
-  bool stopped;                                                                  ///< The stopped state of the chronometer.
-  std::unordered_map<std::string, size_t> cursors; ///< Associating strings to cursors
+  std::vector<std::chrono::high_resolution_clock::time_point> times; ///< All the times taken.
+  bool stopped;                                                      ///< The stopped state of the chronometer.
+  std::unordered_map<std::string, size_t> cursors;                   ///< Associating strings to cursors
+  std::vector<bool> pauses;                                          ///< Associating strings to cursors
   
   /**
    * Get the numeric cursor from its corresponding string.
@@ -46,6 +48,14 @@ protected:
    * @param cursor The name of the cursor saved.
    */
   virtual size_t getNumericCursor(const std::string cursor = "") const;
+  
+  /**
+   * Sum times not in pause.
+   * 
+   * @param fromCursor first cursor to use
+   * @param toCursor last cursor to use
+   */
+  virtual std::chrono::microseconds sumNotPausedTimes(size_t fromCursor, size_t toCursor) const;
 
 public:
   static constexpr std::chrono::microseconds ZEROMS = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::duration::zero()); ///< Zero
@@ -64,9 +74,28 @@ public:
   void start();
   
   /**
-   * Intermediate time taken. It creates a time step without stopping the whole chronometer.
+   * Intermediate time taken.
+   * It creates a time step without stopping the whole chronometer.
+   * If the chronometer has not started, it starts.
    */
   void step(const std::string cursor = "");
+  
+  /**
+   * Intermediate time taken.
+   * It creates a time step pausing the chronometer.
+   * Successive call can be cont or step (with no parameters).
+   * 
+   * @see cont
+   */
+  void pause(const std::string cursor = "");
+  
+  /**
+   * Continues the chronometer after a pause.
+   * If the chronometer has not started, it starts.
+   * 
+   * @see pause
+   */
+  void cont();
   
   /**
    * Last time taken, it stops the chronometer.
@@ -78,6 +107,16 @@ public:
    * It clears the benchmark stat.
    */
   void reset();
+  
+  /**
+   * This method says if the chronometer is currently pausing.
+   */
+  virtual bool isPaused() const;
+  
+  /**
+   * This method says if the chronometer is currently stopped.
+   */
+  virtual bool isStopped() const;
   
   /**
    * Get the benchmark stat in microseconds, that is the time elapsed between the given cursor and its previous time taken.
@@ -101,7 +140,7 @@ public:
    * @param cursor The n-th time taken.
    * @return the time taken in the structure std::chrono::microseconds. You may want to call "count()" on it.
    */
-  std::chrono::microseconds cumulativeBenchmark(const size_t cursor = 0) const;
+  std::chrono::microseconds cumulativeBenchmark(const size_t cursor = 0, const bool noPauses = true) const;
   
   /**
    * Get the benchmark stat in microseconds, that is the time elapsed between the given cursor and the first time taken.
@@ -109,7 +148,28 @@ public:
    * @param cursor The name of the cursor saved.
    * @return the time taken in the structure std::chrono::microseconds. You may want to call "count()" on it.
    */
-  std::chrono::microseconds cumulativeBenchmark(const std::string cursor) const;
+  std::chrono::microseconds cumulativeBenchmark(const std::string cursor, const bool noPauses = true) const;
+  
+  /**
+   * Average time taking into account all steps
+   * 
+   * @return the average time in microseconds.
+   */
+  double average() const;
+  
+  /**
+   * Variance taking into account all steps
+   * 
+   * @return the variance time taken in microseconds squared.
+   */
+  double variance() const;
+  
+  /**
+   * Standard deviation taking into account all steps
+   * 
+   * @return the standard deviation time taken in microseconds.
+   */
+  double standardDeviation() const;
 };
 //-----------------------------------------------------------------------------
 
