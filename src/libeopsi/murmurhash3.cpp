d82e7c6ceb5c7121217e9bf210f0439ad963bcb5
Roberto Metere <roberto@metere.it>
2017-06-07 16:53:47 +0100

    Fixing switch-cases

--- src/libeopsi/murmurhash3.cpp
+++ src/libeopsi/murmurhash3.cpp
@@ -117,11 +117,17 @@ void MurmurHash3::murmurHash3_x86_32 (const void * key, int len, void * out) {
   switch(len & 3) {
     case 3:
       k1 ^= tail[2] << 16;
+      break;
     case 2:
       k1 ^= tail[1] << 8;
+      break;
     case 1:
       k1 ^= tail[0];
-      k1 *= c1; k1 = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+      k1 *= c1;
+      k1 = ROTL32(k1,15);
+      k1 *= c2;
+      h1 ^= k1;
+      break;
     default:
       break;
   };
@@ -159,17 +165,37 @@ void MurmurHash3::murmurHash3_x86_128 (const void * key, const int len, void * o
     uint32_t k3 = getblock32(blocks,i*4+2);
     uint32_t k4 = getblock32(blocks,i*4+3);
 
-    k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
-    h1 = ROTL32(h1,19); h1 += h2; h1 = h1*5+0x561ccd1b;
+    k1 *= c1;
+    k1  = ROTL32(k1,15);
+    k1 *= c2;
+    h1 ^= k1;
+    h1 = ROTL32(h1,19);
+    h1 += h2;
+    h1 = h1*5+0x561ccd1b;
 
-    k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
-    h2 = ROTL32(h2,17); h2 += h3; h2 = h2*5+0x0bcaa747;
+    k2 *= c2;
+    k2  = ROTL32(k2,16);
+    k2 *= c3;
+    h2 ^= k2;
+    h2 = ROTL32(h2,17);
+    h2 += h3;
+    h2 = h2*5+0x0bcaa747;
 
-    k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
-    h3 = ROTL32(h3,15); h3 += h4; h3 = h3*5+0x96cd1c35;
+    k3 *= c3;
+    k3  = ROTL32(k3,17);
+    k3 *= c4;
+    h3 ^= k3;
+    h3 = ROTL32(h3,15);
+    h3 += h4;
+    h3 = h3*5+0x96cd1c35;
 
-    k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
-    h4 = ROTL32(h4,13); h4 += h1; h4 = h4*5+0x32ac3b17;
+    k4 *= c4;
+    k4  = ROTL32(k4,18);
+    k4 *= c1;
+    h4 ^= k4;
+    h4 = ROTL32(h4,13);
+    h4 += h1;
+    h4 = h4*5+0x32ac3b17;
   }
 
   //----------
@@ -184,38 +210,65 @@ void MurmurHash3::murmurHash3_x86_128 (const void * key, const int len, void * o
   switch(len & 15) {
     case 15:
       k4 ^= tail[14] << 16;
+      break;
     case 14:
       k4 ^= tail[13] << 8;
+      break;
     case 13:
       k4 ^= tail[12] << 0;
-      k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
+      k4 *= c4;
+      k4  = ROTL32(k4,18);
+      k4 *= c1;
+      h4 ^= k4;
+      break;
     case 12:
       k3 ^= tail[11] << 24;
+      break;
     case 11:
       k3 ^= tail[10] << 16;
+      break;
     case 10:
       k3 ^= tail[ 9] << 8;
+      break;
     case  9:
       k3 ^= tail[ 8] << 0;
-      k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
+      k3 *= c3;
+      k3  = ROTL32(k3,17);
+      k3 *= c4;
+      h3 ^= k3;
+      break;
     case  8:
       k2 ^= tail[ 7] << 24;
+      break;
     case  7:
       k2 ^= tail[ 6] << 16;
+      break;
     case  6:
       k2 ^= tail[ 5] << 8;
+      break;
     case  5:
       k2 ^= tail[ 4] << 0;
-      k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
+      k2 *= c2;
+      k2  = ROTL32(k2,16);
+      k2 *= c3;
+      h2 ^= k2;
+      break;
     case  4:
       k1 ^= tail[ 3] << 24;
+      break;
     case  3:
       k1 ^= tail[ 2] << 16;
+      break;
     case  2:
       k1 ^= tail[ 1] << 8;
+      break;
     case  1:
       k1 ^= tail[ 0] << 0;
-      k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+      k1 *= c1;
+      k1  = ROTL32(k1,15);
+      k1 *= c2;
+      h1 ^= k1;
+      break;
     default:
       break;
   };
@@ -224,16 +277,24 @@ void MurmurHash3::murmurHash3_x86_128 (const void * key, const int len, void * o
   // finalization
   h1 ^= len; h2 ^= len; h3 ^= len; h4 ^= len;
 
-  h1 += h2; h1 += h3; h1 += h4;
-  h2 += h1; h3 += h1; h4 += h1;
+  h1 += h2;
+  h1 += h3;
+  h1 += h4;
+  h2 += h1;
+  h3 += h1;
+  h4 += h1;
 
   h1 = fmix32(h1);
   h2 = fmix32(h2);
   h3 = fmix32(h3);
   h4 = fmix32(h4);
 
-  h1 += h2; h1 += h3; h1 += h4;
-  h2 += h1; h3 += h1; h4 += h1;
+  h1 += h2;
+  h1 += h3;
+  h1 += h4;
+  h2 += h1;
+  h3 += h1;
+  h4 += h1;
 
   ((uint32_t*)out)[0] = h1;
   ((uint32_t*)out)[1] = h2;
@@ -261,13 +322,23 @@ void MurmurHash3::murmurHash3_x64_128 (const void * key, const int len, void * o
     uint64_t k1 = getblock64(blocks,i*2+0);
     uint64_t k2 = getblock64(blocks,i*2+1);
 
-    k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+    k1 *= c1;
+    k1  = ROTL64(k1,31);
+    k1 *= c2;
+    h1 ^= k1;
 
-    h1 = ROTL64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
+    h1 = ROTL64(h1,27);
+    h1 += h2;
+    h1 = h1*5+0x52dce729;
 
-    k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+    k2 *= c2;
+    k2  = ROTL64(k2,33);
+    k2 *= c1;
+    h2 ^= k2;
 
-    h2 = ROTL64(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
+    h2 = ROTL64(h2,31);
+    h2 += h1;
+    h2 = h2*5+0x38495ab5;
   }
 
   //----------
@@ -280,36 +351,57 @@ void MurmurHash3::murmurHash3_x64_128 (const void * key, const int len, void * o
   switch(len & 15) {
     case 15:
       k2 ^= ((uint64_t)tail[14]) << 48;
+      break;
     case 14:
       k2 ^= ((uint64_t)tail[13]) << 40;
+      break;
     case 13:
       k2 ^= ((uint64_t)tail[12]) << 32;
+      break;
     case 12:
       k2 ^= ((uint64_t)tail[11]) << 24;
+      break;
     case 11:
       k2 ^= ((uint64_t)tail[10]) << 16;
+      break;
     case 10:
       k2 ^= ((uint64_t)tail[ 9]) << 8;
+      break;
     case  9:
       k2 ^= ((uint64_t)tail[ 8]) << 0;
-      k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+      k2 *= c2;
+      k2  = ROTL64(k2,33);
+      k2 *= c1;
+      h2 ^= k2;
+      break;
     case  8:
       k1 ^= ((uint64_t)tail[ 7]) << 56;
+      break;
     case  7:
       k1 ^= ((uint64_t)tail[ 6]) << 48;
+      break;
     case  6:
       k1 ^= ((uint64_t)tail[ 5]) << 40;
+      break;
     case  5:
       k1 ^= ((uint64_t)tail[ 4]) << 32;
+      break;
     case  4:
       k1 ^= ((uint64_t)tail[ 3]) << 24;
+      break;
     case  3:
       k1 ^= ((uint64_t)tail[ 2]) << 16;
+      break;
     case  2:
       k1 ^= ((uint64_t)tail[ 1]) << 8;
+      break;
     case  1:
       k1 ^= ((uint64_t)tail[ 0]) << 0;
-      k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+      k1 *= c1;
+      k1  = ROTL64(k1,31);
+      k1 *= c2;
+      h1 ^= k1;
+      break;
     default:
       break;
   };
